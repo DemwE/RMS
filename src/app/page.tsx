1775a695d2487b9ef6c78484ef56989d228ed758
demwe <mateuszczarnecki360@gmail.com>
2024-03-05 12:49:17 +0100

    Added navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/home/hero"
+import Navigation from "@/components/Navigation";
 
 export default function Home() {
     return (
         <main className="">
+            <Navigation/>
             <Hero/>
         </main>
     );
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ export default function Navigation() {
     return (
         <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/public" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/circle.png" className="h-8" alt="Flowbite Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap">RMS</span>
                 </a>
