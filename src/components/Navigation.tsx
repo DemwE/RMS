89ae0ed22cf6672ee4a0081cb4d9b54d67ad63cf
demwe <mateuszczarnecki360@gmail.com>
2024-03-05 13:08:20 +0100

    Updated navigation and hero section

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 export default function Navigation() {
     return (
-        <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+        <nav className="bg-white w-fullborder-b border-gray-200">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/public" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="/circle.png" className="h-8" alt="Flowbite Logo" />
@@ -11,7 +11,7 @@ export default function Navigation() {
                     <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
                         </svg>
                     </button>
                 </div>
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function hero() {
     return (
-        <section className="h-dvh grid grid-cols-2">
-            <div className="flex justify-center items-center">
+        <section className="grid grid-cols-1 md:grid-cols-2 mx-4">
+            <div className="flex justify-center items-center max-md:order-last">
                 <div className="space-y-4 max-w-72">
                     <h1 className="text-6xl">RMS</h1>
                     <p>Your system for management rankings and players for tournaments</p>
