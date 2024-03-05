c45b5d96d6b5facc2dfa714bda997149bae382ef
demwe <mateuszczarnecki360@gmail.com>
2024-03-05 16:01:14 +0100

    Fixed navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -37,7 +37,7 @@ export default function Navigation() {
                     </button>
                 </div>
                 <div
-                    className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? '' : 'hidden'}`}>
+                    className={`items-center z-10 justify-between w-full md:flex md:w-auto md:order-1 ${isOpen ? '' : 'hidden'}`}>
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white max-md:absolute max-md:right-0 max-md:w-full">
                         <li>
                             <a href="#"
