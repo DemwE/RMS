5bbd309b2af283a139cd13df2e7763d524fb94e5
demwe <mateuszczarnecki360@gmail.com>
2024-03-05 14:25:04 +0100

    Updated footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
                 </span>
                 <ul className="flex flex-wrap justify-center items-center text-sm font-medium text-gray-500 space-x-4">
                     <li>
-                        <a href="https://youtu.be/dQw4w9WgXcQ?si=JobY9R2YdxV592vF"
+                        <a href="https://youtu.be/dQw4w9WgXcQ?si=JobY9R2YdxV592vF" target="_blank"
                            className="text-gray-400 hover:text-gray-500">
                             <Image src="/icons/youtube.svg" alt="youtube" width="24" height="24"/>
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/DemwE/RMS"
+                        <a href="https://github.com/DemwE/RMS" target="_blank"
                            className="text-gray-400 hover:text-gray-500">
                             <Image src="/icons/github.svg" alt="github" width="24" height="24"/>
                         </a>
