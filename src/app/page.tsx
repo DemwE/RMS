6eb1837f95f3c9e0389781e8459239b3283ef22b
demwe <mateuszczarnecki360@gmail.com>
2024-03-05 12:02:32 +0100

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/home/hero"
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>RMS</h1>
+        <main className="">
+            <Hero/>
         </main>
     );
 }
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function hero() {
+    return (
+        <section className="h-dvh grid grid-cols-2">
+            <div className="flex justify-center items-center">
+                <div className="space-y-4 max-w-72">
+                    <h1 className="text-6xl">RMS</h1>
+                    <p>Your system for management rankings and players for tournaments</p>
+                    <div>
+                        <a href="">
+                            <button
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">Check
+                                More
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="flex justify-center items-center">
+                <Image src="/home/stars.png" alt="stars" width="500" height="500"/>
+            </div>
+        </section>
+    )
+}
