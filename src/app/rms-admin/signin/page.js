1bce69f627ead4588a1bf937f4784caa64faf722
demwe <mateuszczarnecki360@gmail.com>
2024-03-06 22:34:10 +0100

    Updated signin

--- src/app/rms-admin/signin/page.js
+++ src/app/rms-admin/signin/page.js
@@ -1,10 +1,7 @@
 'use client'
 import React from "react";
 import signIn from "@/fireauthdb/auth/signin";
-// import LoginForm from "@/components/admin/login-form";
-import { useRouter } from 'next/navigation'
-import {AtSymbolIcon, KeyIcon} from "@heroicons/react/24/outline";
-import {ArrowRightIcon} from "@heroicons/react/20/solid";
+import {useRouter} from 'next/navigation';
 
 function Page() {
     const [email, setEmail] = React.useState('')
@@ -14,7 +11,7 @@ function Page() {
     const handleForm = async (event) => {
         event.preventDefault()
 
-        const { result, error } = await signIn(email, password);
+        const {result, error} = await signIn(email, password);
 
         if (error) {
             return console.log(error)
@@ -25,96 +22,49 @@ function Page() {
         return router.push("/rms-admin")
     }
     return (
-        <main className='flex items-center justify-center md:h-screen'>
-            <div className='relative mx-auto flex w-full max-w[400px] flex-coll space-y-2.5 p-4 md:h-[500px] md:-mt-32'>
-                <div className='flex h-20 w-full justify-center items-center rounded-lg bg-blue-500 p-3 md:h-full'>
-                    {/*<LoginForm onSubmit={handleForm}*/}
-                    {/*           onEmailChange={(e) => setEmail(e.target.value)}*/}
-                    {/*           onPasswordChange={(e) => setPassword(e.target.value)}*/}
-                    {/*/>*/}
-                    <form className="space-y-3" onSubmit={handleForm}>
-                        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-                            <h1 className={`mb-3 text-2xl`}>
-                                Please log in to continue
-                            </h1>
-                            <div className="w-full">
-                                <div>
-                                    <label
-                                        className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                                        htmlFor="email"
-                                    >
-                                        Email
-                                    </label>
-                                    <div className="relative">
-                                        <input
-                                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                            id="username"
-                                            type="email"
-                                            name="username"
-                                            placeholder="Enter your email address"
-                                            required
-                                            onChange={(e) => setEmail(e.target.value)}
-                                        />
-                                        <AtSymbolIcon
-                                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
-                                    </div>
-                                </div>
-                                <div className="mt-4">
-                                    <label
-                                        className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                                        htmlFor="password"
-                                    >
-                                        Password
-                                    </label>
-                                    <div className="relative">
-                                        <input
-                                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                            id="password"
-                                            type="password"
-                                            name="password"
-                                            placeholder="Enter password"
-                                            required
-                                            minLength={6}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                        />
-                                        <KeyIcon
-                                            className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
-                                    </div>
-                                </div>
+        <main className="bg-gray-50">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <a href="/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
+                    <img className="w-10 h-10 mr-2" src="/circle.png"
+                         alt="logo"/>
+                    RMS
+                </a>
+                <div
+                    className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                            Sign in to your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleForm}>
+                            <div>
+                                <label htmlFor="email"
+                                       className="block mb-2 text-sm font-medium text-gray-900">Your
+                                    email</label>
+                                <input type="email" name="email" id="email"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5"
+                                       placeholder="name@company.com"
+                                       onChange={(e) => setEmail(e.target.value)}
+                                       required/>
                             </div>
-                            <button className="mt-4 w-full">
-                                <p className='inline-block'>Log in&nbsp;</p>
-                                <ArrowRightIcon className="inline-block ml-auto h-5 w-5 text-black"/>
+                            <div>
+                                <label htmlFor="password"
+                                       className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-700 focus:border-blue-700 block w-full p-2.5"
+                                       minLength={6}
+                                       onChange={(e) => setPassword(e.target.value)}
+                                       required/>
+                            </div>
+                            <button type="submit"
+                                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign
+                                in
                             </button>
-                            <div
-                                className="flex h-8 items-end space-x-1"
-                                aria-live="polite"
-                                aria-atomic="true"
-                            >
-                            </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </main>
     )
-    // return (<div className="wrapper">
-    //     <div className="form-wrapper">
-    //         <h1 className="mt-60 mb-30">Sign up</h1>
-    //         <form onSubmit={handleForm} className="form">
-    //             <label htmlFor="email">
-    //                 <p>Email</p>
-    //                 <input onChange={(e) => setEmail(e.target.value)} required type="email" name="email" id="email" placeholder="example@mail.com" />
-    //             </label>
-    //             <label htmlFor="password">
-    //                 <p>Password</p>
-    //                 <input onChange={(e) => setPassword(e.target.value)} required type="password" name="password" id="password" placeholder="password" />
-    //             </label>
-    //             <button type="submit">Sign up</button>
-    //         </form>
-    //     </div>
-    //
-    // </div>);
 }
 
 export default Page;
